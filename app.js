9d27545d67d089f849704168e88c6a970a5aea68
githubber2047 <110272418+githubber2047@users.noreply.github.com>
2023-09-25 03:51:30 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
+window.addEventListener('load', async () => {
+    if (window.ethereum) {
+        window.web3 = new Web3(ethereum);
+        try {
+            await ethereum.enable();
+            startApp();
+        } catch (error) {
+            console.error("User denied account access...")
+        }
+    } else if (window.web3) {
+        window.web3 = new Web3(web3.currentProvider);
+        startApp();
+    } else {
+        console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
+    }
+});
 
+async function startApp() {
+    const accounts = await web3.eth.getAccounts();
+    const networkId = await web3.eth.net.getId();
+    const deployedNetwork = ExchangeRateBetting.networks[networkId];
+    const instance = new web3.eth.Contract(
+        ExchangeRateBetting.abi,
+        deployedNetwork && deployedNetwork.address,
+    );
+
+    const placeBetButton = document.querySelector('button');
+    placeBetButton.addEventListener('click', async () => {
+        const betRate = document.getElementById('betRate').value;
+        const above = document.getElementById('above').value === 'true';
+        await instance.methods.placeBet(betRate, above).send({ from: accounts[0], value: web3.utils.toWei('1', 'ether') });
+    });
+
+    const settleBetButton = document.querySelector('button');
+    settleBetButton.addEventListener('click', async () => {
+        await instance.methods.settleBet().send({ from: accounts[0] });
+    });
+}
